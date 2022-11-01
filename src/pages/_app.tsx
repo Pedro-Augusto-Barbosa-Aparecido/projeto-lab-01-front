5aa1cb47c8612481c7aafe811ae444fece144499
Pedro-Augusto-Barbosa-Aparecido <77130441+Pedro-Augusto-Barbosa-Aparecido@users.noreply.github.com>
2022-10-31 22:13:34 -0300

    configuring styled-components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,19 @@
 import type { AppProps } from "next/app";
+import Head from "next/head";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyles } from "../styles/global";
+import { defaultTheme } from "../styles/themes/defaults";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div>
+      <Head>
+        <title>POnk Objects</title>
+      </Head>
+      <ThemeProvider theme={defaultTheme}>
+        <Component {...pageProps} />
+        <GlobalStyles />
+      </ThemeProvider>
+    </div>
+  );
 }
